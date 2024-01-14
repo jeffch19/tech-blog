347bfc8929d5318ef963dfb33286af4ec0c1faa9
jeffch19 <17jcummings@gmail.com>
2024-01-13 20:16:26 -0500

    added dashboard into layouts

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,34 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // Wait for the DOM to be fully loaded
-
-  const signupForm = document.getElementById('signupForm');
-
-  signupForm.addEventListener('submit', async (event) => {
+  const signupForm = document.getElementById("signupForm");
+  signupForm.addEventListener("submit", async (event) => {
     event.preventDefault(); // Prevent the default form submission behavior
-
     // Get the values from the form
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
     try {
       // Send a POST request to your server with the signup data
-      const response = await fetch('/api/signup', {
-        method: 'POST',
+      const response = await fetch("/api/user", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ username, password }),
       });
-
       if (response.ok) {
         // Redirect to the signin page after successful signup
-        window.location.href = '/signin';
+        window.location.href = "/";
       } else {
         // Handle errors, e.g., display an error message
-        console.error('Error during signup:', response.statusText);
+        console.error("Error during signup:", response.statusText);
       }
     } catch (error) {
-      console.error('Error during signup:', error);
+      console.error("Error during signup:", error);
     }
   });
 });
