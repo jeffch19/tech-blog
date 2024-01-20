9f6250a3f7df43d72aaf98dc36024d876c725175
jeffch19 <17jcummings@gmail.com>
2024-01-20 17:52:13 -0500

    added handlebar updates

--- seeders/postSeeds.js
+++ seeders/postSeeds.js
@@ -2,25 +2,21 @@ const { Post } = require('../models');
 
 const postData = [
 {
-    title: "the great foot",
-    content: `In the silence of the night, unseen footsteps echo with a haunting cadence, as shadows beneath the feet whisper tales of forgotten souls. A chilling breeze carries the weight of untold secrets, stirring the very ground beneath, as if the earth itself shudders at the impending march of spectral soles`,
+    title: "Model",
+    content: `The HTML is the "skeleton" of bedrock content. Text that communicates information to the reader.`,
     userId: 1
 },
 {
-    title: "the great face",
-    content: `Faces obscured in the dim glow of twilight bear the haunting etchings of forgotten nightmares, each wrinkle a chapter of ancient sorrow waiting to be unveiled. In the shadows, masks of stoic facade conceal the abyss within, where the eyes harbor a darkness that mirrors the unfathomable depths of a forgotten abyss.`,
+    title: "View",
+    content: `The CSS adds visual style to the content. It is the "skin" that we use to flesh out our skeleton and give it a particular look. We can swap in different skins via CSS without altering the original content in any way. They are relatively, but not completely, independent.`,
     userId: 2
 },
 {
-    title: "the great fungus",
-    content: `Beneath the veil of decaying foliage, an ominous fungus sprawls, its mycelium weaving a sinister network that whispers secrets to the earth. As the fungal tendrils slither through the soil, a silent pact with nature unfolds, promising a symbiosis that carries an eerie undertone of unseen consequences.`,
+    title: "Controller",
+    content: `The browser is responsible for combining and rendering the CSS and HTML into a set of final, manipulatible pixels on the screen. It gathers input from the user and marshals it to any JavaScript code necessary for the page to function. But here, too, we have flexibility: we can plug in a different brower and get comparable results. Some browsers might render it faster, or with more fidelity, or with more bells and whistles. `,
     userId: 3
 },
-{
-    title: "the great hotdog",
-    content: `In the sizzling embrace of the grill, hot dogs contort into sinister forms, their charred exteriors concealing a mystery that grins beneath the bun. A spectral aroma wafts through the air, as if the very essence of their creation holds a pact with the culinary shadows, imparting an ominous flavor to those who dare to indulge.`,
-    userId: 4
-},
+
 ];
 
 const seedPosts = () => Post.bulkCreate(postData);
